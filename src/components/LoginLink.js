ec2b297444e1ea3b59f5aef4e6788190641d0d3c
behnam <behnam.main@gmail.com>
2020-07-08 23:37:15 +0430

    first commit-folder structure

---
+++ src/components/LoginLink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginLink = () => {
+  return (
+    <div>
+      login link
+    </div>
+  )
+}
+
+export default LoginLink
---
+++ src/pages/Checkout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Checkout = () => {
+  return (
+    <div>
+      Ckeck out
+    </div>
+  )
+}
+
+export default Checkout
